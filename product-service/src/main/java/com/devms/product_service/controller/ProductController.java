cc6f132ff42ee6d68e398ee1018496a33c72e9d3
Manmohan Kushwaha <mkush575@gmail.com>
2026-01-20 01:43:41 +0530

    Add logging for product creation in ProductController

--- product-service/src/main/java/com/devms/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/devms/product_service/controller/ProductController.java
@@ -5,6 +5,8 @@ import com.devms.product_service.dto.ProductResponse;
 import com.devms.product_service.model.Product;
 import com.devms.product_service.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
+@Slf4j
 public class ProductController {
 
     private final ProductService productService;
@@ -22,6 +25,7 @@ public class ProductController {
     public void createProduct(@RequestBody ProductRequest productRequest)
     {
         productService.createProduct(productRequest);
+        log.info("Product created: {}", productRequest);
     }
 
     @GetMapping
